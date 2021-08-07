7ffa0d39280850a6f7d09062a41df0c14ac0be5f
Ivan <ivan.zderadicka@gmail.com>
2021-08-07 11:49:39 +0200

    add data samples

---
+++ data/heroes.sql
@@ -0,0 +1,10 @@
+insert into heroes (id, name) values (11,'Dr Nice');
+insert into heroes (id, name) values (13,'Bombasto');
+insert into heroes (id, name) values (12,'Narco');
+insert into heroes (id, name) values (14,'Celeritas');
+insert into heroes (id, name) values (15,'Magneta');
+insert into heroes (id, name) values (16,'RubberMan');
+insert into heroes (id, name) values (17,'Dynama');
+insert into heroes (id, name) values (18,'Dr IQ');
+insert into heroes (id, name) values (19,'Magma');
+insert into heroes (id, name) values (20,'Tornado');
